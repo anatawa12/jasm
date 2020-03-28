4eec40388746fa0d837c53a6d620b6fbdcbeebd9
anatawa12 <anatawa12@icloud.com>
2020-03-28 15:40:24 +0900

    fix plugin to support 4.x

--- jasm-gradle-plugin/src/main/kotlin/com/anatawa12/jasm/plugins/gradle/JasmPlugin.kt
+++ jasm-gradle-plugin/src/main/kotlin/com/anatawa12/jasm/plugins/gradle/JasmPlugin.kt
@@ -3,16 +3,16 @@ package com.anatawa12.jasm.plugins.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.internal.HasConvention
+import org.gradle.api.internal.file.FileResolver
 import org.gradle.api.plugins.JavaPlugin
-import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.SourceSetContainer
-import java.io.File
+import javax.inject.Inject
 
-open class JasmPlugin : Plugin<Project> {
+open class JasmPlugin @Inject constructor(val resolver: FileResolver) : Plugin<Project> {
     override fun apply(project: Project) {
         project.plugins.apply(JavaPlugin::class.java)
         project.convention.getByType(SourceSetContainer::class.java).all { sourceSet ->
-            val extension = JasmSourceSetExtension(project.objects)
+            val extension = JasmSourceSetExtension(project, resolver)
             (sourceSet as HasConvention).convention.add("jasm", extension)
             extension.jasm.srcDirs(project.projectDir.resolve("src/${sourceSet.name}/jasm"))
             extension.jasm.srcDirs(project.projectDir.resolve("src/${sourceSet.name}/java"))
--- jasm-gradle-plugin/src/main/kotlin/com/anatawa12/jasm/plugins/gradle/JasmSourceSetExtension.kt
+++ jasm-gradle-plugin/src/main/kotlin/com/anatawa12/jasm/plugins/gradle/JasmSourceSetExtension.kt
@@ -3,12 +3,40 @@ package com.anatawa12.jasm.plugins.gradle
 import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.file.SourceDirectorySet
+import org.gradle.api.internal.file.DefaultSourceDirectorySet
+import org.gradle.api.internal.file.FileResolver
 import org.gradle.api.model.ObjectFactory
+import java.lang.reflect.Constructor
 
-open class JasmSourceSetExtension(factory: ObjectFactory) {
-    val jasm: SourceDirectorySet = factory.sourceDirectorySet("jasm", "jasm")
+open class JasmSourceSetExtension(project: Project, resolver: FileResolver) {
+    val jasm: SourceDirectorySet = createDefaultSourceDirectorySet(project, "jasm", resolver)
 
     fun jasm(block: Action<SourceDirectorySet>) {
         block.execute(jasm)
+    }
+
+    companion object {
+        private fun createDefaultSourceDirectorySet(project: Project, name: String, resolver: FileResolver): SourceDirectorySet {
+            val klass = DefaultSourceDirectorySet::class.java
+            val defaultConstructor = klass.constructorOrNull(String::class.java, FileResolver::class.java)
+
+            return if (defaultConstructor != null && defaultConstructor.getAnnotation(java.lang.Deprecated::class.java) == null) {
+                // TODO: drop when gradle < 2.12 are obsolete
+                defaultConstructor.newInstance(name, resolver)
+            } else {
+                val directoryFileTreeFactoryClass = Class.forName("org.gradle.api.internal.file.collections.DirectoryFileTreeFactory")
+                val alternativeConstructor = klass.getConstructor(String::class.java, FileResolver::class.java, directoryFileTreeFactoryClass)
+
+                val defaultFileTreeFactoryClass = Class.forName("org.gradle.api.internal.file.collections.DefaultDirectoryFileTreeFactory")
+                val defaultFileTreeFactory = defaultFileTreeFactoryClass.getConstructor().newInstance()
+                alternativeConstructor.newInstance(name, resolver, defaultFileTreeFactory)
+            }
+        }
+        private fun <T> Class<T>.constructorOrNull(vararg parameterTypes: Class<*>): Constructor<T>? =
+            try {
+                getConstructor(*parameterTypes)
+            } catch (e: NoSuchMethodException) {
+                null
+            }
     }
 }
