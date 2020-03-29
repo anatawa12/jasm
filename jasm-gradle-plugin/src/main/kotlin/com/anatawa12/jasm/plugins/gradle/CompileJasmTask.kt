5e6f4a9779cde154da50df26278f23d9b474fa6a
anatawa12 <anatawa12@icloud.com>
2020-03-29 11:40:06 +0900

    fix: handle internal errors

--- jasm-gradle-plugin/src/main/kotlin/com/anatawa12/jasm/plugins/gradle/CompileJasmTask.kt
+++ jasm-gradle-plugin/src/main/kotlin/com/anatawa12/jasm/plugins/gradle/CompileJasmTask.kt
@@ -4,9 +4,11 @@ import com.anatawa12.jasm.assembleJasmFile
 import com.anatawa12.jasm.assembler.AssemblerOptions
 import com.anatawa12.jasm.parseJasmFile
 import com.anatawa12.jasm.verifyFile
+import org.apache.groovy.json.internal.Exceptions
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.*
 import java.io.File
+import java.util.*
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
@@ -30,18 +32,34 @@ open class CompileJasmTask : DefaultTask() {
         hadError = false
         val outputDir = files.outputDir
 
+        val errors = Collections.synchronizedList(mutableListOf<Exception>())
+
         for (file in files) {
-            pool.submit {
-                if (!compileFile(file, outputDir))
-                    hadError = true
+            pool.execute {
+                try {
+                    println("compiling $file")
+                    if (!compileFile(file, outputDir))
+                        hadError = true
+                    println("compiled $file")
+                } catch (e: Exception) {
+                    errors.add(RuntimeException("compiling $file", e))
+                }
             }
         }
 
         pool.shutdown()
         while (!pool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)){}
 
+        if (errors.isNotEmpty()) {
+            throw RuntimeException("some errors has occurred").apply {
+                for (error in errors) {
+                    addSuppressed(error)
+                }
+            }
+        }
+
         if (hadError)
-            throw RuntimeException("some compile error has occeed")
+            throw RuntimeException("some compile error has occurred")
     }
 
     private fun compileFile(file: File, outputDir: File): Boolean {
